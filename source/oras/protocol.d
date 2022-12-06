689abcd22a8ce0583f99d611f3cf100ed19d13e4
Sebastiaan Koppe <mail@skoppe.eu>
2022-12-06 14:16:49 +0100

    cleanup

--- source/oras/protocol.d
+++ source/oras/protocol.d
@@ -84,7 +84,7 @@ struct Manifest {
   string mediaType;
   Layer[] layers;
   string[string] annotations;
-  Config config = Config("application/vnd.oci.image.config.v1+json", Digest("sha256", "44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a"), 2);
+  Config config;
 }
 
 struct ManifestResponse {
