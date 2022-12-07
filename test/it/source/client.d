33f8c939dac390aea882b2b940de99550dccfe1d
Sebastiaan Koppe <mail@skoppe.eu>
2022-12-07 22:45:20 +0100

    Make locations in tests relative

--- test/it/source/client.d
+++ test/it/source/client.d
@@ -32,7 +32,8 @@ import mir.algebraic;
   auto result = session.upload(toBlob([1,2,3,4]))
     .get!(UploadResult);
 
-  result.location.should == "http://localhost:5000/v2/stuff/faz/blobs/sha256:cf97adeedb59e05bfd73a2b4c2a8885708c4f4f70c84c64b27120e72ab733b72";
+  result.location.shouldEndWith("/v2/stuff/faz/blobs/sha256:cf97adeedb59e05bfd73a2b4c2a8885708c4f4f70c84c64b27120e72ab733b72");
+  result.digest.toString.should == "sha256:cf97adeedb59e05bfd73a2b4c2a8885708c4f4f70c84c64b27120e72ab733b72";
 }
 
 @("storeManifest.ok")
@@ -48,15 +49,15 @@ import mir.algebraic;
   auto stored = client
     .storeManifest(oras.client.Name("stuff/foo"), Reference(Tag("v1.2.4")), manifest).get!(ManifestResult);
 
-  stored.location.should == "http://localhost:5000/v2/stuff/foo/manifests/sha256:06efd1c8b55e8b012ea5314d6cf2150b33c55aaa8ea0cd6d106eccf58854ad58";
-  stored.digest.should == "sha256:06efd1c8b55e8b012ea5314d6cf2150b33c55aaa8ea0cd6d106eccf58854ad58";
+  stored.location.shouldEndWith("/v2/stuff/foo/manifests/sha256:06efd1c8b55e8b012ea5314d6cf2150b33c55aaa8ea0cd6d106eccf58854ad58");
+  stored.digest.toString().should == "sha256:06efd1c8b55e8b012ea5314d6cf2150b33c55aaa8ea0cd6d106eccf58854ad58";
 }
 
 @("hasBlob.false")
 @safe unittest {
   auto client = Client(Config("http://localhost:5000"));
   auto query = client
-    .hasBlob(oras.client.Name("stuff/baz"), Digest.from("sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a").assumeOk)
+    .hasBlob(oras.client.Name("stuff/baz"), Digest.from("sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8b").assumeOk)
     .get!(bool);
 
   query.should == false;
@@ -84,8 +85,8 @@ import mir.algebraic;
     .upload(oras.client.Name("stuff/bla"), toBlob(bytes))
     .get!(UploadResult);
 
-  result.location.should == "http://localhost:5000/v2/stuff/bla/blobs/sha256:9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a";
-  result.digest.should == "sha256:9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a";
+  result.location.shouldEndWith("/v2/stuff/bla/blobs/sha256:9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a");
+  result.digest.toString().should == "sha256:9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a";
 }
 
 @("upload.chunks")
@@ -98,8 +99,8 @@ import mir.algebraic;
     .upload(oras.client.Name("stuff/foz"), toBlob(cs))
     .get!(UploadResult);
 
-  result.location.should == "http://localhost:5000/v2/stuff/foz/blobs/sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72";
-  result.digest.should == "sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72";
+  result.location.shouldEndWith("/v2/stuff/foz/blobs/sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72");
+  result.digest.toString().should == "sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72";
 }
 
 @("startChunkedUpload.blob")
@@ -119,5 +120,17 @@ import mir.algebraic;
   auto result = session.finish()
     .get!(UploadResult);
 
-  result.location.should == "http://localhost:5000/v2/stuff/faz/blobs/sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72";
+  result.location.shouldEndWith("/v2/stuff/faz/blobs/sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72");
+  result.digest.toString.should == "sha256:66840dda154e8a113c31dd0ad32f7f3a366a80e8136979d8f5a101d3d29d6f72";
+}
+
+void shouldEndWith(V, E)(auto ref V value, auto ref E expected, string file = __FILE__, size_t line = __LINE__)
+{
+  import mir.format : text;
+  import std.algorithm : endsWith;
+  if (!value.endsWith(expected))
+    {
+      const msg = text("Expected   : ", expected, "\n", "to end with: ", value);
+      throw new UnitTestException(msg, file, line);
+    }
 }
